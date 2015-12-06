b6bfd1c2730231500b8d672c71b9c7a3056da169
Yao Li <myspiritcrazy@gmail.com>
2015-12-06 14:01:34 -0800

    set the initial location

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -12,9 +12,21 @@ import MapKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // set initial location in honolulu
+        let initLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(initLocation)
+    }
+
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
 }
