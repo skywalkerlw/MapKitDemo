871a863690b8fbe1dd19ee3aee35085a8d19c03b
Yao Li <myspiritcrazy@gmail.com>
2015-12-06 15:37:08 -0800

    to show multiple pins in map

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -23,13 +23,16 @@ class ViewController: UIViewController {
         let initLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         centerMapOnLocation(initLocation)
 
-        // show artwork on map
-        let artwork = Artwork(title: "King David Kalakaua",
-                locationName: "Waikiki Gateway Park",
-                discipline: "Sculpture",
-                coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        loadInitialData()
+        mapView.addAnnotations(artworks)
 
-        mapView.addAnnotation(artwork)
+        // show artwork on map
+//        let artwork = Artwork(title: "King David Kalakaua",
+//                locationName: "Waikiki Gateway Park",
+//                discipline: "Sculpture",
+//                coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+//
+//        mapView.addAnnotation(artwork)
         mapView.delegate = self
     }
 
