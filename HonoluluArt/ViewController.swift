f128e4fc98614d4a6281202d557e1aa73fad210a
Yao Li <myspiritcrazy@gmail.com>
2015-12-06 14:22:08 -0800

    add artwork class

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -21,6 +21,14 @@ class ViewController: UIViewController {
         // set initial location in honolulu
         let initLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         centerMapOnLocation(initLocation)
+
+        // show artwork on map
+        let artwork = Artwork(title: "King David Kalakaua",
+                locationName: "Waikiki Gateway Park",
+                discipline: "Sculpture",
+                coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+
+        mapView.addAnnotation(artwork)
     }
 
     func centerMapOnLocation(location: CLLocation) {
